24a2f930055dbd1eec07b5debeb52face622e9b8
ahmed <ahmedslashabdulkarim@gmail.com>
2026-03-17 16:25:25 +0100

    update Class AsterixController

---
+++ src/main/java/org/example/asterixaapi/model/CharacterRepository.java
@@ -0,0 +1,11 @@
+/*
+package org.example.asterixaapi.model;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.lang.Character;
+
+public interface CharacterRepository extends MongoRepository<Character, String> {
+}
+
+ */
